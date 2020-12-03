d047066ab433ce3ddeb77fa5c22ce26617659093
aramaisay <aramaisyeghiazaryan@gmail.com>
2020-12-03 13:43:22 -0800

    transfered functionality of Main from ref based to state

--- src/Components/DropBox.js
+++ src/Components/DropBox.js
@@ -5,15 +5,15 @@ import { kitchenTypeSelector } from '../Redux/Selectors'
 
 import '../Css/Components/DropBox.css';
 
-const DropBox = forwardRef(({}, ref) => {
+const DropBox = ({onChange}) => {
     const data = useSelector(kitchenTypeSelector)
     return(
-        <select className = 'dropCont' name = 'kitchenTypes' ref = {ref}>
+        <select className = 'dropCont' name = 'kitchenTypes' onChange = {onChange} >
             {data.map((item) => {
                 return <option key = {item.abbr} className = 'dropOpt' value = {item.abbr} >{item.name}</option>
             })}
         </select>
     );
-});
+};
 
 export default DropBox;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useFetch from '../Hooks/useFetch';
 import SearchBox from '../Components/SearchBox';
@@ -8,22 +8,28 @@ import RestourantList from '../Components/RestourantList';
 import '../Css/Pages/Main.css';
 
 const Main = () => {
-    const dropBoxRef = useRef();
+    const [text, setText] = useState('');
+    const [cuisine, setCuisine] = useState('arm');
     const {isLoading, data, fetchData} = useFetch();
 
-    useEffect(() => {
-        fetchData('resSearch', {kitchenType: dropBoxRef.current.value, text: ''});
-    }, [])
 
-    const onChange = (e) => {
-        fetchData('resSearch', {kitchenType: dropBoxRef.current.value, text: e.target.value});
+    useEffect(() => {
+        fetchData('resSearch', {kitchenType: cuisine, text: text});
+    }, [text, cuisine])
+    
+    const onChangeText = (e) => {
+        setText(e.target.value);
+    }
+
+    const onChangeDropBox = (e) => {
+        setCuisine(e.target.value);
     }
 
     return (
         <div className = 'mainCont' >
             <div className = 'searchCont' >
-                <SearchBox onChange = {onChange} placeholder = 'Search for the available restourants'  />
-                <DropBox ref = {dropBoxRef} />
+                <SearchBox onChange = {onChangeText} placeholder = 'Search for the available restourants'  />
+                <DropBox onChange = {onChangeDropBox}  />
             </div>
             <RestourantList data = {data} isLoading = {isLoading} ></RestourantList>
         </div>
